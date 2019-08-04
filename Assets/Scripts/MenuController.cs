650c0cd877d4281125e012a2b2c88e096e06bfa3
Andy <burnsan@mail.gvsu.edu>
2019-08-04 00:44:32 -0400

    Added Main Menu and Credits

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    
+    public int selected_index = 0;
+
+    public Text menu;
+    /* 
+    public Color default_color;
+    public Color selected_color;
+    */
+    private List<string> button_list;
+
+    private string[] left_select = {"||", "|>|", "|>>|", "|>>>|", "|>>>>|", "|>>>>>|"};
+    private string[] right_select = {"||", "|<|", "|<<|", "|<<<|", "|<<<<|", "|<<<<<|"};
+
+    public int hold_interval = 0;
+
+    public float hold_time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button_list = new List<string>();
+
+        button_list.Add("Play");
+        button_list.Add("Credits");
+        button_list.Add("Exit");
+        
+
+        //foreach(Text t in GetComponentsInChildren<Text>()) buttonList.Add(t);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        int index = 0;
+        foreach(Text t in buttonList)
+        {
+            if(index == selected_index) t.color = selected_color;
+            else t.color = default_color;
+
+            index++;
+        }
+        */
+        
+        
+
+        if ((Input.GetKeyUp("space")))
+        {
+            Debug.Log(hold_interval.ToString());
+            if (hold_interval >= 3)
+            {
+                Debug.Log("Selected!");
+                CheckSelection();
+            }
+            else SelectNextItem();
+        }
+         
+        if ((Input.GetKey("space"))) IncreaseInterval();
+        else
+        {
+            hold_interval = 0;
+            hold_time = 0;
+        }
+
+        UpdateTextBox();
+    }
+
+
+    void UpdateTextBox()
+    {
+        string menu_text = "";
+        int index = 0;
+        foreach(string s in button_list)
+        {
+            if(index == selected_index) menu_text += "\n" + left_select[hold_interval] + s + right_select[hold_interval];
+            else menu_text += "\n" + s;
+
+            index++;
+        }
+
+        menu.text = menu_text;
+    }
+
+    void SelectNextItem()
+    {
+        selected_index++;
+        if(selected_index > button_list.Count - 1) selected_index = 0;
+    }
+
+    void IncreaseInterval()
+    {
+        //if (hold_interval == 0) hold_time = 0;
+
+        hold_time += Time.deltaTime;
+
+        hold_interval = (int)(hold_time * 2);
+
+        if (hold_interval > 3) hold_interval = 3;
+    }
+
+    void CheckSelection()
+    {
+        Debug.Log("CheckSelection fired.");
+        switch(selected_index)
+        {
+            case 0:
+                //Play Game
+                Debug.Log("Play Selected");
+                //SceneManager.LoadScene("Gamescene");
+                break;
+            case 1:
+                //Credits
+                Debug.Log("Credits Selected");
+                break;
+            case 2:
+                //Exit
+                Application.Quit();
+                Debug.Log("Quit Selected");
+                break;
+        }
+    }
+}
