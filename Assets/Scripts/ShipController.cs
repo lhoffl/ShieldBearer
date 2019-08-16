3826dbc258388c6a7056f2e9401317401d640d2d
Matthew Hoffman <matt@lhoffl.com>
2019-08-16 15:47:52 -0400

    Player Movement Added
    Player can now move using the wasd keys, moving disables the shield. The shield takes time to come back online after moving. Changed enemy colliders from circle to polygon.

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -6,19 +6,24 @@ public class ShipController : MonoBehaviour
 {
     public float duration = 1f;
     public float range_mod = 50;
+    public float speed = 0.035f;
+    public int moving_cooldown = 40;
 
     public AudioClip shipSFX_clip;
 
     private AudioSource shipSFX_source;
     
+    private GameObject shield;
 
     private float amplitude = 0;
-
-    private int count = 0;
+    private bool moving = false;
+    private int time_count = 0;
+    private int shield_count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        shield = GameObject.FindGameObjectWithTag("Shield");
         shipSFX_source = AddAudio(shipSFX_clip, true, true, 0.2f);
         shipSFX_source.Play();
 
@@ -27,19 +32,47 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // NO
-        //if ((Input.GetKey("up"))) changePos(0, 0.1f);
-        //if (Input.GetKey("down")) changePos(0,-0.1f);
+        // FINE
+        if ((Input.GetKey("w"))) {
+            changePos(0, speed);
+            moving = true;
+        }
+        if (Input.GetKey("s")) {
+            changePos(0,-speed);
+            moving = true;
+        }
+        if (Input.GetKey("a")) {
+            changePos(-speed,0);
+            moving = true;
+        }
+        if (Input.GetKey("d")) {
+            changePos(speed,0);
+            moving = true;
+        }
 
-        ListAimlessly();
+        if ((Input.GetKeyUp("w")) || Input.GetKeyUp("s") || Input.GetKeyUp("a") || Input.GetKeyUp("d")) {
+            moving = false;
+        }
+
+        // Disable the shield when moving
+        if(moving) {
+            shield.SetActive(false);
+            shield_count = 0;
+        } else if(!moving && moving_cooldown != shield_count) {
+            shield_count++;
+        } else if(!moving && moving_cooldown == shield_count) {
+            shield.SetActive(true);
+        }
+
+        //ListAimlessly();
 
         changePos(0, (amplitude/range_mod));
 
-        if(count == 60) {
+        if(time_count == 60) {
             GameObject.FindGameObjectWithTag("GameManager").GetComponent<ScoreController>().IncrementScore(1);
-            count = 0;
+            time_count = 0;
         } else {
-            count++;
+            time_count++;
         }
 
         /* 
