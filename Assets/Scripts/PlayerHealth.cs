0d78e164967425c19b9637a6dbe80e2f209ffd7f
Matthew Hoffman <matt@lhoffl.com>
2019-08-16 20:02:15 -0400

    Added health packs and items

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -8,10 +8,12 @@ public class PlayerHealth : MonoBehaviour {
     private Vector2 screen_area = GameManager.GetScreenArea();
     public Vector2 healthbar_location;
     public int healthbar_width = 20;
-    public int health = 200;
+    public int max_health = 200;
+    public int health;
     public Vector2 healthbar;
 
     void Start() {
+        health = max_health;
         healthbar_location = new Vector2(screen_area.x/2 + buffer_area, screen_area.y + buffer_area);
         healthbar = new Vector2(health, healthbar_width);
     }
@@ -33,6 +35,16 @@ public class PlayerHealth : MonoBehaviour {
             GameObject.FindGameObjectWithTag("GameManager").GetComponent<ScoreController>().ResetScore();
             SceneManager.LoadScene(sceneName:"GameOver");
         }
+        healthbar.x = health;
+    }
+
+    public void IncrementHealthBar(int amount) {
+        if(health >= max_health) {
+            GameObject.FindGameObjectWithTag("GameManager").GetComponent<ScoreController>().IncrementScore(100);
+            return;
+        }
+
+        health += amount;
         healthbar.x = health;
     }
 }
